5997df61da35261f8144f19a40ceed20a258f645
Yamani AL_tamimi <yamani5006@gmail.com>
2024-11-16 00:01:11 +0300

    POV:adding the animation for welcome page

--- pizza_app/lib/Welcome.dart
+++ pizza_app/lib/Welcome.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
-
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'LoginScreen.dart';
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: (Image.asset('assets/images/logo.png')),
-        
-      ),
+return AnimatedSplashScreen(
+      splash: Image.asset('assets/images/logo.png',width:10000,height: 100000,),
+      nextScreen: LoginPage(),
+      splashTransition: SplashTransition.fadeTransition,
+      duration: 4000,
+      splashIconSize: 400,
     );
   }
 }
+
+
+  
--- pizza_app/lib/main.dart
+++ pizza_app/lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:pizza_app/LoginScreen.dart';
 import 'package:pizza_app/Welcome.dart';
-
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home:Scaffold(appBar:AppBar(backgroundColor: Colors.red[400],
-    title:Text('Pizza App',style: TextStyle(color: Colors.white),)
+    title:Text('Pizza App',style: TextStyle(color: Colors.white),),
   
-  
-  ,) ,
 
-  body:LoginPage(),
+  
+  ) ,
+  body:Welcome() ,
   )
   )
 
