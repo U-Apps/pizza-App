0c177f156e4dee0662c0c24356753ef00aea3c93
Abo Night <ahmdalqhlany@gmail.com>
2024-11-17 13:53:17 +0300

    Add Account Page

--- pizza_app/lib/main.dart
+++ pizza_app/lib/main.dart
@@ -4,18 +4,19 @@ import 'package:pizza_app/Register.dart';
 import 'package:pizza_app/Welcome.dart';
 import 'package:pizza_app/home.dart';
 import 'package:pizza_app/successfulLogin.dart';
+import 'package:pizza_app/Profile.dart';
+
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:Scaffold(appBar:AppBar(backgroundColor: Colors.red[400],
-    title:Text('Pizza App',style: TextStyle(color: Colors.white),),
-  
-
-  
-  ) ,
-  body:Home(),
-  )
-  )
-
-  );
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red[400],
+          title: Text(
+            'Pizza App',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: AccountPage(),
+      )));
 }
